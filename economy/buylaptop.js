41bb3b2ec43ac0978c7352f4bbe4e211814126e6
Glitch (lizard-outstanding-catmint) <none>
2024-02-24 13:18:32 +0000

    ⏲⛳️ Checkpoint ./economy/buylaptop.js:65339275/412

--- economy/buylaptop.js
+++ economy/buylaptop.js
@@ -2,8 +2,8 @@ module.exports = {
   
   name: "buy-laptop",
   code: `
-$description[**@$username Success in buying a Laptop💻**]
+$description[**@$username** successfully bought a **fishing rod** for **$13000**. Remaining cash: $$sub[$getGlobalUserVar[cash]]]
 $setGlobalUserVar[cash;$sub[$getGlobalUserVar[cash];13000]]
 $setGlobalUserVar[laptop;$sum[$getGlobalUserVar[laptop];1]]
-$onlyIf[$getGlobalUserVar[cash]>=13000;Not enough cash!]`
+$onlyIf[$getGlobalUserVar[cash]>=13000;Not enough cash! You need $13000 to buy a fishing rod, but you only have $$getGlobalUserVar[cash].]`
 }
