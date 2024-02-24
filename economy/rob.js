d5713411fee826cd6c2ec2da9423820a1a0cad37
Glitch (lizard-outstanding-catmint) <none>
2024-02-24 19:52:33 +0000

    🌃📰 Checkpoint ./economy/rob.js:65339275/18

--- economy/rob.js
+++ economy/rob.js
@@ -8,13 +8,13 @@ $else
 $if[$random[1;100]<=81]
 $setGlobalUserVar[cash;$sub[$getGlobalUserVar[cash;$mentioned[1]];100];$mentioned[1]]
 $setGlobalUserVar[cash;$sum[$getGlobalUserVar[cash];100]]
-$description[You stole a small portion from $username[$mentioned[1]] out of his balance. You may now go buy a gum you little poor boy.]
+$description[You stole a small portion from $username[$mentioned[1]] out of his balance. You may now go buy a gum you little poor boy. ($100)]
 $color[00FF00]
 $else
 $if[$random[1;100]<=99]
 $setGlobalUserVar[cash;$sub[$getGlobalUserVar[cash;$mentioned[1]];500];$mentioned[1]]
 $setGlobalUserVar[cash;$sum[$getGlobalUserVar[cash];500]]
-$description[You stole a respectable amount of cash from $username[$mentioned[1]]. Keep them carefully.]
+$description[You stole a respectable amount of cash from $username[$mentioned[1]]. Keep them carefully. ($500)]
 $color[00FF00]
 $else
 $setGlobalUserVar[cash;0;$mentioned[1]]
