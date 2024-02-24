e91d17e5b3c45ef8712015bf8b9d319110440b28
Glitch (lizard-outstanding-catmint) <none>
2024-02-24 20:15:26 +0000

    🚴🍫 Checkpoint ./economy/rob.js:65339275/14530

--- economy/rob.js
+++ economy/rob.js
@@ -1,39 +1,32 @@
 module.exports = {
   name: "rob",
   code: `
-$let[robChance;$random[1;100]]
-$let[target;$mentioned[1]]
-$let[robber;$authorID]
-$let[targetCash;$getGlobalUserVar[cash;$get[target]]]
-$let[robAmount;0]
-$if[$get[target]!=$get[robber]]
-  $if[$get[robChance]<=25]
-    $description[Bro really failed. Skill issue lol]
-    $color[FF0000]
-  $elseif[$get[robChance]<=81]
-    $let[robAmount;$truncate[$multiply[$get[targetCash];0.05]]]
-    $setGlobalUserVar[cash;$sub[$get[targetCash];$get[robAmount]];$get[target]]
-    $setGlobalUserVar[cash;$sum[$getGlobalUserVar[cash;$get[robber]];$get[robAmount]];$get[robber]]
-    $description[You stole a small portion from $username[$get[target]] out of his balance. (5% - $get[robAmount])]
-    $color[00FF00]
-  $elseif[$get[robChance]<=99]
-    $let[robAmount;$truncate[$multiply[$get[targetCash];0.15]]]
-    $setGlobalUserVar[cash;$sub[$get[targetCash];$get[robAmount]];$get[target]]
-    $setGlobalUserVar[cash;$sum[$getGlobalUserVar[cash;$get[robber]];$get[robAmount]];$get[robber]]
-    $description[You stole a respectable amount of cash from $username[$get[target]]. (15% - $get[robAmount])]
-    $color[00FF00]
-  $else
-    $let[robAmount;$get[targetCash]]
-    $setGlobalUserVar[cash;0;$get[target]]
-    $setGlobalUserVar[cash;$sum[$getGlobalUserVar[cash;$get[robber]];$get[robAmount]];$get[robber]]
-    $description[YOU STOLE HIS WHOLE FORTUNE!!! LMFAO!!! YOU RICH AND THEY BROKIE!!!]
-    $color[0000FF]
-  $endif
+$cooldown[60s;WOW THERE, NOT SO FAST, THERE IS 1 MINUTE COOLDOWN YK? (%time% left)]
+$if[$random[1;100]<=25]
+$description[Bro really failed. Skill issue lol]
+$color[FF0000]
 $else
-  $description[No way you actually attempted to rob yourself 💀]
-  $color[FF0000]
+$if[$random[1;100]<=81]
+$setGlobalUserVar[cash;$sub[$getGlobalUserVar[cash;$mentioned[1]];100];$mentioned[1]]
+$setGlobalUserVar[cash;$sum[$getGlobalUserVar[cash];100]]
+$description[You stole a small portion from $username[$mentioned[1]] out of his balance. You may now go buy a gum you little poor boy. ($100)]
+$color[00FF00]
+$else
+$if[$random[1;100]<=99]
+$setGlobalUserVar[cash;$sub[$getGlobalUserVar[cash;$mentioned[1]];500];$mentioned[1]]
+$setGlobalUserVar[cash;$sum[$getGlobalUserVar[cash];500]]
+$description[You stole a respectable amount of cash from $username[$mentioned[1]]. Keep them carefully. ($500)]
+$color[00FF00]
+$else
+$setGlobalUserVar[cash;0;$mentioned[1]]
+$setGlobalUserVar[cash;$sum[$getGlobalUserVar[cash];$getGlobalUserVar[cash;$mentioned[1]]]]
+$description[Jackpot! You stole all of $username[$mentioned[1]]'s cash!]
+$color[0000FF]
 $endif
-$onlyIf[$get[target]!=;You need to mention someone to rob!]
+$endif
+$endif
+$onlyIf[$mentioned[1]!=$authorID;No way you actually attempted to rob yourself 💀]
+$onlyIf[$mentioned[1]!=;You need to mention someone to rob!]
 $addTimestamp
 `
 };
